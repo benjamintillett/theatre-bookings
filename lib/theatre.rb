f0ed6d1c50551776610198647a217ff070e78b3b
benjamin tillett <benjamin.tillett@gmail.com>
2014-11-06 11:34:43 +0000

    add working available method and remove redundant adjacent seat methods

--- lib/theatre.rb
+++ lib/theatre.rb
@@ -35,6 +35,13 @@ class Theatre
 		end
 	end 
 
+	def clear_below?(coordinates)
+	 	if relative_seat_empty?(coordinates,-1) && !relative_seat_empty?(coordinates,-2)
+	 		false
+		else
+			true
+		end
+	end 
 
 	def relative_seat_empty?(coordinates, displacement)
 		return false unless relative_seat(coordinates,displacement)
@@ -47,29 +54,12 @@ class Theatre
 		nil 
 	end
 
-	def adjacent_seats(coordinates)
-		adjacent_seats = []
-		adjacent_seats << [coordinates[0],coordinates[1] - 1 ] if is_valid_seat?([coordinates[0], coordinates[1] - 1])
-		adjacent_seats << [coordinates[0],coordinates[1] + 1 ] if is_valid_seat?([coordinates[0], coordinates[1] + 1])
-	end
-
-	def two_seats_above(coordinates)
-		two_seats_above = []
-		two_seats_above << [coordinates[0],coordinates[1] + 1 ] if is_valid_seat?([coordinates[0], coordinates[1] + 1])
-		two_seats_above << [coordinates[0],coordinates[1] + 1 ] if is_valid_seat?([coordinates[0], coordinates[1] + 2])
-	end
-
-
-
-	def booking_creates_lonely_seat?
-	end
-
 	def has_empty_adjacent?(coordinates)
 		adjacent_seats(coordinates).map{ |seat| seat_empty?(seat) }.reduce(:|)		
 	end
 
 	def is_available?(coordinates)
-		has_empty_adjacent?(coordinates) && seat_empty?(coordinates)
+		clear_below?(coordinates) && clear_above?(coordinates) && seat_empty?(coordinates) 
 	end
 
 
--- spec/theatre_spec.rb
+++ spec/theatre_spec.rb
@@ -59,29 +59,6 @@ describe Theatre do
 
 	end
 
-	context "dealing with adjacent seats" do 
-
-		before do 
-			the_roundhouse.book_seat([0,2])
-			the_roundhouse.book_seat([0,4])
-		end
-
-		let(:lonely_seat) { [0,3] }
-
-		it "given coordinates can retutn the coordinates of adjacent seats" do 
-			expect(the_roundhouse.adjacent_seats([0,1])).to eq([[0,0],[0,2]])
-		end
-
-
-		it "knows [0,1] has an empty adjacent seat" do 
-			expect(the_roundhouse.has_empty_adjacent?([0,1])).to eq(true)
-		end
-
-		it "knows [0,3] has no empty adjacent seats" do 
-			expect(the_roundhouse.has_empty_adjacent?(lonely_seat)).to eq(false)
-		end
-
-	end
 
 	context "seat availability in a non empty theatre" do 
 
@@ -94,6 +71,10 @@ describe Theatre do
 			expect(the_roundhouse.is_available?([0,5])).to eq true
 		end		
 
+		it "knows [0,0] is unavailabe" do 
+			expect(the_roundhouse.is_available?([0,0])).to eq false
+		end
+
 	end
 
 	it "knows wheather booking the seat create a single unbooked seat above" do 
@@ -101,13 +82,19 @@ describe Theatre do
 		expect(the_roundhouse.clear_above?([0,0])).to eq false		
 	end
 
-	it "knows that a booking that sanwiches a empty spaces to end of row is invalid" do 
+	it "knows wheather booking the seat create a single unbooked seat above" do 
 		expect(the_roundhouse.clear_above?([0,48])).to eq false		
 	end
 
-	it "knows that a booking that sanwiches a empty spaces to beggining of row is invalid" do
-
+	it "knows that a booking that sandwiches an empty seat to the end of the row is unailable" do
+		expect(the_roundhouse.is_available?([0,48])).to eq false		
 	end 
+
+	it "knows that a booking that sandwiches an empty seat to the beggingng of the row is unailable" do
+		expect(the_roundhouse.is_available?([0,1])).to eq false		
+	end 
+
+
 
 
 
